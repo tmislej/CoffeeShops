d1d4c8d2623537784394579c9c96c40608648a30
Tine Mislej <tine.mislej@gmail.com>
2017-04-11 10:45:44 +0200

    map

--- app/src/main/java/com/tine/coffeeshops/ui/main/MainActivity.java
+++ app/src/main/java/com/tine/coffeeshops/ui/main/MainActivity.java
@@ -4,18 +4,66 @@ import android.os.Bundle;
 
 import com.tine.coffeeshops.R;
 import com.tine.coffeeshops.ui.base.DaggerActivity;
+import com.tine.coffeeshops.ui.main.map.CoffeeShopsMapView;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends DaggerActivity<MainComponent> implements MainMvp.View {
 
+    @BindView(R.id.map_view) CoffeeShopsMapView mapView;
+
     @Inject MainMvp.Presenter presenter;
+
+    // region activity callbacks
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mapView.onCreate(savedInstanceState);
     }
+
+    @Override protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    // endregion activity callbacks
 
     @Override protected MainComponent inject() {
         MainComponent component = DaggerMainComponent.builder()
